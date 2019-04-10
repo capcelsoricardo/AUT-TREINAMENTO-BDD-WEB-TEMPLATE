4b4b5fec105d9fd5c9ba787c322f033b3bcc3a0d
XB137077 <p000ceferreira@prservicos.com.br>
2019-04-10 17:07:24 -0300

    Arquivo Properties e Alteracao do Hooks

--- src/test/java/curso/treinamento/setup/Hooks.java
+++ src/test/java/curso/treinamento/setup/Hooks.java
@@ -1,8 +1,12 @@
 package curso.treinamento.setup;
 
 
+import java.util.ResourceBundle;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
@@ -11,14 +15,30 @@ import curso.treinamento.utils.Helper;
 
 public class Hooks {
 	
+	public static ResourceBundle bundle =  ResourceBundle.getBundle("application");
+	
 	public static WebDriver driver;
 
 	@Before
 	public void startTest(Scenario scenario) {
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/mac/chromedriver");
-	
-		driver = new ChromeDriver();
-		driver.get("http://newtours.demoaut.com/");
+		
+		switch (bundle.getString("env.navegador").toUpperCase()) {
+			
+			case "CHROME":
+				System.setProperty("webdriver.chrome.driver", bundle.getString("env.path.driver") + "chromedriver");
+				driver = new ChromeDriver();
+			break;
+			
+			case "SAFARI": 
+				driver = new SafariDriver();
+		default:
+			break;
+		}
+		
+		driver.get(bundle.getString("env.url.application"));
+		
+		if(bundle.getString("env.window.maximize").trim().equalsIgnoreCase("true") )
+			driver.manage().window().fullscreen();
 	}
 
 	@After
